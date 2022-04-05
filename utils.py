122990640dcd18af5555c38fef5b4e61a87d6f2f
Stanley Araki <stanley.araki@gmail.com>
2022-04-05 14:07:00 -0400

    Added axis to cartpole score graph

--- utils.py
+++ utils.py
@@ -3,11 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_learning_curve(x, scores, figure_file):
+def plot_learning_curve(x, scores, figure_file, x_label, y_label):
     running_avg = np.zeros(len(scores))
     for i in range(len(running_avg)):
         running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
     plt.plot(x, running_avg)
     plt.title('Running average of previous 100 scores')
     plt.savefig(figure_file)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
 
